4657288b9221fffa4dde4bc58ff44a92e0dabaa4
Fedot Praslov <fedot@praslov.com>
2016-02-16 12:26:18 +0200

    Trying npm2 & npm3 location of uap-core package. Version bump

--- index.js
+++ index.js
@@ -1,10 +1,14 @@
 var path = require('path'),
     fs = require('fs'),
     yaml = require('yamlparser'),
-    refImpl = require('uap-ref-impl')(readYAML('node_modules/uap-core/regexes.yaml'));
+    regexes_base_path = 'node_modules/uap-core/regexes.yaml',
+    uap_regexes_path = [
+        path.join(__dirname, regexes_base_path),
+        path.join(__dirname, '../..', regexes_base_path)
+    ].find(fs.existsSync),
+    refImpl = require('uap-ref-impl')(readYAML(uap_regexes_path));
 
-function readYAML(fileName) {
-    var file = path.join(__dirname, fileName);
+function readYAML(file) {
     var data = fs.readFileSync(file, 'utf8');
     return yaml.eval(data);
 }
